15a11e40d25859b5533de258840d9315a44ceb21
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-12-27 12:41:19 -0500

    able to add items and show them but using inheritance i.e putting child in parent div and accessing the items

--- scripts/app.js
+++ scripts/app.js
@@ -2,94 +2,25 @@
     "use strict";
     let app = angular.module("ngApp", []);
     app
-        .controller("squareController", SquareController)
-        .controller("digestController", digestController)
-        .controller("eatController", eatController)
-        .controller("parentController",parentController)
-        .controller("childController",childController)
-        .filter("upperCase", upperCaseFilterFactory)
-        .filter("squareNumber", squareNumberFactory)
-        .filter("truth", truthFactory)
+        .controller("shoppingListAddController", ShoppingListAddController)
+        .controller("shoppingListShowController", ShoppingListShowController)
     
-    app.$inject = ["$scope", "$filter", "upperCaseFilter", "$timeout"]
+    app.$inject = ["$scope", "$timeout"]
 
-    let desserts = ["Jalebi", "IceCream", "Gulabjamun", "Bhatisa", "Rasgulla", "Milkcake"];
-    let catering = [
-        {
-            quantity: 2,
-            name: "Shahi Paneer"
-        },
-        {
-            quantity: 4,
-            name: "Dal Makhni"
-        },
-        {
-            quantity: 3,
-            name: "Palak Paneer"
-        },
-        {
-            quantity: 2,
-            name: "Chicken Tikka"
-        }
-    ]
-    function eatController($scope) {
-        $scope.desserts = desserts;
-        $scope.catering = catering;
-    }
-    function digestController($scope,$timeout) {
-        $scope.counterValue = 0;
-        //$scope.lname = "Last name"
-        $scope.add = function () {
-            //console.log(++$scope.counterValue);
-            $timeout(() => {
-                console.log(++$scope.counterValue)
-                // $scope.$digest()
-            }, 3000);
-        }
-        $scope.show = function () {
-            console.log($scope.$$watchersCount)
+    function ShoppingListAddController($scope) {
+        let slac = this;
+        slac.item = ""
+        slac.quantity = 0;
+        slac.list = [];
+        slac.addItem = function(){
+           //slac.item && console.log(slac.item + " " + slac.quantity)
+           slac.list.push({item:slac.item,quant:slac.quantity})
+           console.log(slac.list)
         }
     }
-    function SquareController($scope, $filter, upperCaseFilter, squareNumberFilter) {
-        $scope.number = 0
-        $scope.name = ""
-        $scope.changeName = function () {
-            console.log(upperCaseFilter($scope.name))
-        }
+    function ShoppingListShowController($scope) {
+        let slsc = this;
+        slsc.name = ["karan","singh","sodhi"];
     }
-    function squareNumberFactory() {
-        return function (input) {
-            return Math.pow(input, 2);
-        }
-    }
-    function truthFactory() {
-        return function (input, target, replace) {
-            input = input || "";
-            input = input.replace(target, replace);
-            return input;
-        }
-    }
-    function upperCaseFilterFactory($filter) {
-        return function (input) {
-            return $filter("uppercase")(input);
-        }
-    } 
-    function parentController($scope){
-        // $scope.parentValue = 99
-        // $scope.pc = this;
-        // $scope.pc.parentValue = 7;
-        let parent = this;
-        parent.value = 70;
-    }
-    function childController($scope){
-        let child = this;
-        child.value = 140;
-        // $scope.pc = this
-        // $scope.parentValue = 11
-        // console.log($scope.$parent.pc.parentValue)
-        // $scope.pc.parentValue = 123;
-        // console.log('$scope.parentValue is : ' + $scope.parentValue)
-        // console.log($scope)
-        // console.log($scope.$parent.pc.parentValue)
-    }
+    
 })()
