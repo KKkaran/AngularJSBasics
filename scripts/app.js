890d2fd29a2f19cdb084e5b80e55d58b7e3d558e
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-12-28 22:30:00 -0500

    able to create the instance with using provider using service

--- scripts/app.js
+++ scripts/app.js
@@ -3,13 +3,25 @@
     
     let app = angular.module("ngApp",[]);
     app 
-      .controller("Shoppinglist1", ShoppingList1)
+     // .controller("Shoppinglist1", ShoppingList1)
       .controller("Shoppinglist2", ShoppingList2)
-      .factory("ShoppingListFactory", ShoppingListFactory);
+     // .factory("ShoppingListFactory", ShoppingListFactory);
+      .provider("ShoppingListService",ShoppingListServiceProvider);
 
-    ShoppingList1.$inject = ["ShoppingListFactory"]
-    ShoppingList2.$inject = ["ShoppingListFactory"]
+    //ShoppingList1.$inject = ["ShoppingListFactory"]
+    ShoppingList2.$inject = ["ShoppingListService"]
 
+    function ShoppingListServiceProvider(){
+        let provider = this;
+        provider.defaults = {
+            maxItems : 10
+        }
+        provider.$get = function(){
+            let sl = new ShoppingListService(provider.defaults.maxItems)
+            return sl;
+        }
+
+    }
     function ShoppingListFactory(){
         return function(maxItems){
             return new ShoppingListService(maxItems);
@@ -33,29 +45,29 @@
             return items;
         }
     }
-    function ShoppingList1(ShoppingListFactory){
-        let sl1 = this;
-        let service = ShoppingListFactory();
+    // function ShoppingList1(ShoppingListFactory){
+    //     let sl1 = this;
+    //     let service = ShoppingListFactory();
 
-        sl1.item = "";
-        sl1.quantity = "";
-        sl1.items = service.getItems();
+    //     sl1.item = "";
+    //     sl1.quantity = "";
+    //     sl1.items = service.getItems();
         
-        sl1.addItem = function(){
-            service.addItem(sl1.item,sl1.quantity)
-        }
-    }
-    function ShoppingList2(ShoppingListFactory){
+    //     sl1.addItem = function(){
+    //         service.addItem(sl1.item,sl1.quantity)
+    //     }
+    // }
+    function ShoppingList2(ShoppingListService){
         let sl2 = this;
-        let service = ShoppingListFactory(2);
+        //let service = ShoppingListFactory(2);
 
         sl2.item = "";
         sl2.quantity = "";
-        sl2.items = service.getItems();
+        sl2.items = ShoppingListService.getItems();
         
         sl2.addItem = function(){
             try{
-                service.addItem(sl2.item,sl2.quantity)
+                ShoppingListService.addItem(sl2.item,sl2.quantity)
             }catch(error){
                 sl2.error = error.message
             }
