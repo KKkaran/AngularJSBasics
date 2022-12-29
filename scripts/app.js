2e8639807878e60f5654ef84ec059a438218dbb6
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-12-28 22:35:01 -0500

    reformated code

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,22 @@
 (function(){
     "use strict";
-    
     let app = angular.module("ngApp",[]);
     app 
      // .controller("Shoppinglist1", ShoppingList1)
       .controller("Shoppinglist2", ShoppingList2)
      // .factory("ShoppingListFactory", ShoppingListFactory);
       .provider("ShoppingListService",ShoppingListServiceProvider);
-
     //ShoppingList1.$inject = ["ShoppingListFactory"]
     ShoppingList2.$inject = ["ShoppingListService"]
-
     function ShoppingListServiceProvider(){
         let provider = this;
         provider.defaults = {
-            maxItems : 10
+            maxItems : 3
         }
         provider.$get = function(){
             let sl = new ShoppingListService(provider.defaults.maxItems)
             return sl;
         }
-
     }
     function ShoppingListFactory(){
         return function(maxItems){
@@ -30,7 +26,6 @@
     function ShoppingListService(maxItems){
         let service = this;
         let items = [];
-
         service.addItem = function(item,quantity){
             if((maxItems == undefined) || (maxItems !== undefined) && (items.length < maxItems)){
                 items.push({
@@ -60,18 +55,15 @@
     function ShoppingList2(ShoppingListService){
         let sl2 = this;
         //let service = ShoppingListFactory(2);
-
         sl2.item = "";
         sl2.quantity = "";
         sl2.items = ShoppingListService.getItems();
-        
         sl2.addItem = function(){
             try{
                 ShoppingListService.addItem(sl2.item,sl2.quantity)
             }catch(error){
                 sl2.error = error.message
-            }
-            
+            }  
         }
     }
 })()
