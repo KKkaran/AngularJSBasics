db09aed1cf23fa9c0e6853dc6bb124e8d12c1d35
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-12-30 17:03:23 -0500

    working on the service

--- scripts/app.js
+++ scripts/app.js
@@ -1,75 +1,142 @@
-(function(){
-    "use strict";
-    let app = angular.module("ngApp",[]);
-    app 
-     // .controller("Shoppinglist1", ShoppingList1)
-      .controller("Shoppinglist2", ShoppingList2)
-     // .factory("ShoppingListFactory", ShoppingListFactory);
-      .provider("ShoppingListService",ShoppingListServiceProvider)
-      .config(Config)
-      
-    Config.$inject = ['ShoppingListServiceProvider']
-    function Config(ShoppingListServiceProvider){
-        ShoppingListServiceProvider.defaults.maxItems = 3;
-    }
-    //ShoppingList1.$inject = ["ShoppingListFactory"]
-    ShoppingList2.$inject = ["ShoppingListService"]
-    function ShoppingListServiceProvider(){
-        let provider = this;
-        provider.defaults = {
-            maxItems : 3
-        }
-        provider.$get = function(){
-            let sl = new ShoppingListService(provider.defaults.maxItems)
-            return sl;
-        }
-    }
-    function ShoppingListFactory(){
-        return function(maxItems){
-            return new ShoppingListService(maxItems);
-        }
-    }
-    function ShoppingListService(maxItems){
+(function () {
+    'use strict'
+    let app = angular.module("ngApp", [])
+    app
+      .controller("shoppingList1",shoppingList1)
+      .service("shoppingService",shoppingService)
+    app.$inject = ['shoppingService']
+
+    function shoppingService() {
         let service = this;
-        let items = [];
-        service.addItem = function(item,quantity){
-            if((maxItems == undefined) || (maxItems !== undefined) && (items.length < maxItems)){
+        let items = []
+        service.addItem = function(item,quantity) {
+            if (maxItems == undefined || (maxItems !== undefined && items.length < maxItems)) {
                 items.push({
                     item: item,
                     quantity: quantity
                 })
-            }else{
-                throw new Error("Max items (" + maxItems + ") reached.")
+                console.log(items)
+            } else {
+                throw new Error("max item(s) " + maxItems + " reached.")
             }
         }
-        service.getItems = function(){
+        service.getItems = function () {
             return items;
         }
     }
-    // function ShoppingList1(ShoppingListFactory){
-    //     let sl1 = this;
-    //     let service = ShoppingListFactory();
+    
+    function shoppingList1(shoppingService) {
+        let sl1 = this;
+        sl1.item = "";
+        sl1.quantity = "";
 
-    //     sl1.item = "";
-    //     sl1.quantity = "";
-    //     sl1.items = service.getItems();
-        
-    //     sl1.addItem = function(){
-    //         service.addItem(sl1.item,sl1.quantity)
-    //     }
-    // }
-    function ShoppingList2(ShoppingListService){
-        let sl2 = this;
-        //let service = ShoppingListFactory(2);
-        sl2.item = "";
-        sl2.quantity = "";
-        sl2.items = ShoppingListService.getItems();
-        sl2.addItem = function(){
-            try{
-                ShoppingListService.addItem(sl2.item,sl2.quantity)
-            }catch(error){
-                sl2.error = error.message
-            }  
+        sl1.items = shoppingService.getItems();
+
+        sl1.addItem = function () {
+            //shoppingFactory.addItem()
+            sl1.item && sl1.quantity && function () {
+                shoppingService.addItem(sl1.item, sl1.quantity)
+            }()
         }
     }
 })()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (function(){
+//     "use strict";
+//     let app = angular.module("ngApp",[]);
+//     app 
+//      // .controller("Shoppinglist1", ShoppingList1)
+//       .controller("Shoppinglist2", ShoppingList2)
+//      // .factory("ShoppingListFactory", ShoppingListFactory);
+//       .provider("ShoppingListService",ShoppingListServiceProvider)
+//       .config(Config)
+      
+//     Config.$inject = ['ShoppingListServiceProvider']
+//     function Config(ShoppingListServiceProvider){
+//         ShoppingListServiceProvider.defaults.maxItems = 3;
+//     }
+//     //ShoppingList1.$inject = ["ShoppingListFactory"]
+//     ShoppingList2.$inject = ["ShoppingListService"]
+//     function ShoppingListServiceProvider(){
+//         let provider = this;
+//         provider.defaults = {
+//             maxItems : 3
+//         }
+//         provider.$get = function(){
+//             let sl = new ShoppingListService(provider.defaults.maxItems)
+//             return sl;
+//         }
+//     }
+//     function ShoppingListFactory(){
+//         return function(maxItems){
+//             return new ShoppingListService(maxItems);
+//         }
+//     }
+//     function ShoppingListService(maxItems){
+//         let service = this;
+//         let items = [];
+//         service.addItem = function(item,quantity){
+//             if((maxItems == undefined) || (maxItems !== undefined) && (items.length < maxItems)){
+//                 items.push({
+//                     item: item,
+//                     quantity: quantity
+//                 })
+//             }else{
+//                 throw new Error("Max items (" + maxItems + ") reached.")
+//             }
+//         }
+//         service.getItems = function(){
+//             return items;
+//         }
+//     }
+//     // function ShoppingList1(ShoppingListFactory){
+//     //     let sl1 = this;
+//     //     let service = ShoppingListFactory();
+
+//     //     sl1.item = "";
+//     //     sl1.quantity = "";
+//     //     sl1.items = service.getItems();
+        
+//     //     sl1.addItem = function(){
+//     //         service.addItem(sl1.item,sl1.quantity)
+//     //     }
+//     // }
+//     function ShoppingList2(ShoppingListService){
+//         let sl2 = this;
+//         //let service = ShoppingListFactory(2);
+//         sl2.item = "";
+//         sl2.quantity = "";
+//         sl2.items = ShoppingListService.getItems();
+//         sl2.addItem = function(){
+//             try{
+//                 ShoppingListService.addItem(sl2.item,sl2.quantity)
+//             }catch(error){
+//                 sl2.error = error.message
+//             }  
+//         }
+//     }
+// })()
