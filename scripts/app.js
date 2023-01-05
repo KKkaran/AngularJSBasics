294fbb9b62c5e94a338299892c626a88a5b94ed3
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2023-01-05 01:20:37 -0500

    using = and @ to isolate scope

--- scripts/app.js
+++ scripts/app.js
@@ -13,7 +13,13 @@
     ShoppingList2.$inject = ["ShoppingListFactory"]
     function listItem() {
         return {
-            templateUrl: "listItem.html"
+            templateUrl: "listItem.html",
+            scope: {
+                valthruat: "@",
+                valthruequal: "=",
+                list: '=myList',
+                title:"@titles"
+            }
         }
     }
     function listItemDescription() {
@@ -48,10 +54,12 @@
     function ShoppingList1(ShoppingListFactory){
         let sl1 = this;
         let service = ShoppingListFactory();
-
+        sl1.valthruat = "value through @"
+        sl1.valthruequal = "value through ="
+        sl1.title = "Shooping List #1"
         sl1.item = "";
         sl1.quantity = "";
-        sl1.items = service.getItems();
+        sl1.items = service.getItems(); 
         
         sl1.addItem = function(){
             service.addItem(sl1.item,sl1.quantity)
@@ -60,7 +68,7 @@
     function ShoppingList2(ShoppingListFactory){
         let sl2 = this;
         let service = ShoppingListFactory(2);
-
+        sl2.title = "Shooping List #2"
         sl2.item = "";
         sl2.quantity = "";
         sl2.items = service.getItems();
